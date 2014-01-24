4de6d8c44590bd8790d0a3a9dd7a67cf4f03f749
ethanpang <ethan.c.pang@gmail.com>
2014-01-25 03:06:29 +0800

    test

---
+++ local/logs/log-2014-01-23.php
@@ -0,0 +1,42 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-01-23 17:42:44 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-23 17:42:44 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-23 17:42:44 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:42:44 --> Could not find the language definition "HOME_META_KEYWORD_EN_US"
+ERROR - 2014-01-23 17:42:44 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:43:02 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-23 17:43:02 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-23 17:43:02 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:43:02 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-23 17:43:02 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-23 17:43:02 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:43:02 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:43:07 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-23 17:43:07 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-23 17:43:07 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:43:07 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:43:07 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-23 17:43:07 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-23 17:43:07 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:45:48 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-23 17:45:48 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-23 17:45:48 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:45:48 --> Could not find the language definition "HOME_META_KEYWORD_EN_US"
+ERROR - 2014-01-23 17:45:48 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:46:10 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-23 17:46:10 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-23 17:46:10 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 17:46:10 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 19:09:23 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-23 19:09:23 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-23 19:09:23 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 19:09:23 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 19:10:15 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-23 19:10:15 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-23 19:10:15 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 19:10:15 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 19:18:35 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-23 19:18:35 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-23 19:18:35 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-23 19:18:35 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
---
+++ local/logs/log-2014-01-24.php
@@ -0,0 +1,56 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-01-24 03:49:00 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 03:49:00 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 03:49:00 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:49:00 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:52:38 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 03:52:38 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 03:52:38 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:52:38 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:52:49 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 03:52:49 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 03:52:49 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:52:49 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:53:00 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 03:53:00 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 03:53:00 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:53:00 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:53:13 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 03:53:13 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 03:53:13 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:53:14 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:53:29 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 03:53:29 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 03:53:29 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:53:29 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:53:29 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 03:53:29 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 03:53:29 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 03:53:29 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 15:57:26 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 15:57:26 --> Severity: Notice  --> mysql_pconnect(): send of 5 bytes failed with errno=32 Broken pipe /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 15:57:26 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 15:57:27 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 15:57:27 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 19:33:34 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 19:33:34 --> Severity: Notice  --> mysql_pconnect(): send of 5 bytes failed with errno=32 Broken pipe /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 19:33:34 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 19:33:34 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 19:33:34 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 19:33:36 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 19:33:36 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 19:33:36 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 19:33:36 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 19:33:38 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 19:33:38 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 19:33:38 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 19:33:38 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 19:33:39 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 19:33:39 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 19:33:39 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 19:33:39 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 19:33:41 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /opt/lampp/htdocs/Cart/system/codeigniter/database/drivers/mysql/mysql_driver.php 98
+ERROR - 2014-01-24 19:33:41 --> Could not find the language definition "USE_DEFAULT_LANGUAGE_CURRENCY"
+ERROR - 2014-01-24 19:33:41 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
+ERROR - 2014-01-24 19:33:41 --> Could not find the language definition "SERVICES_CATEGORY_PATH_CALCULATE_PRODUCT_COUNT"
